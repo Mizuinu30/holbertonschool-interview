debe297f9e8da585a8b4579e7c45b69ad6d16ef2
Mizuinu30 <juansilva.dvm@gmail.com>
2024-06-12 21:35:05 -0400

    initial commit

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,44 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "palindrome.h"
 
-typedef struct listint_s
+/**
+ * is_palindrome - checks whether or not a given unsigned integer is a
+ *	palindrome
+ */
+int is_palindrome(unsigned long n)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+    int num = n;
+    int digit, rev_num;
 
-/* Function prototypes */
-int is_palindrome(listint_t **head);
-listint_t *reverse_list(listint_t *head);
-int compare_lists(listint_t *head1, listint_t *head2);
-void print_list(listint_t *head);
-
-/* Function to check if a singly linked list is a palindrome */
-int is_palindrome(listint_t **head)
-{
-    if (head == NULL || *head == NULL)
-        return 1;
-
-    listint_t *slow = *head, *fast = *head;
-    listint_t *first_half, *second_half, *second_half_reversed;
-
-    /* Find the middle of the list */
-    while (fast != NULL && fast->next != NULL)
+    rev_num = 0;
+    while (n > 0)
     {
-        slow = slow->next;
-        fast = fast->next->next;
+        digit = n % 10;
+        rev_num = (rev_num * 10) + digit;
+        n /= 10;
     }
-
-    /* Reverse the second half of the list */
-    second_half = slow;
-    second_half_reversed = reverse_list(second_half);
-
-    /* Compare the first half with the reversed second half */
-    first_half = *head;
-    int result = compare_lists(first_half, second_half_reversed);
-
-    /* Restore the list (optional) */
-    reverse_list(second_half_reversed);
-
-    return result;
+    if (rev_num == num)
+        return (0);
+    return (1);
 }
