4ac4379039943d572c3859b1250161b40572cda0
Mizuinu30 <juansilva.dvm@gmail.com>
2024-07-19 13:55:09 -0400

    initial commit

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -1,7 +1,7 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
-#include <stddef.h> // Include stddef.h for size_t
+#include <stddef.h> /* Include stddef.h for size_t */
 
 #define SLIDE_LEFT  0
 #define SLIDE_RIGHT 1
@@ -9,4 +9,3 @@
 int slide_line(int *line, size_t size, int direction);
 
 #endif /* SLIDE_LINE_H */
-// Your code will be compiled with this header file
