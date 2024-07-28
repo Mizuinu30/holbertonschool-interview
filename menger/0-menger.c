e621d0c475cded8bc25508faab8c227a3c0cfb0c
Mizuinu30 <juansilva.dvm@gmail.com>
2024-07-28 18:28:53 -0400

    update import

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -2,40 +2,35 @@
 #include <stdio.h>
 #include <math.h>
 
-// Helper function to determine if a specific cell should be filled or empty
+/* Helper function to determine if a specific cell should be filled or empty */
 int isCellFilled(int x, int y)
 {
     while (x > 0 || y > 0)
     {
         if (x % 3 == 1 && y % 3 == 1)
         {
-            return 0; // center cell should be empty
+            return 0; /* center cell should be empty */
         }
         x /= 3;
         y /= 3;
     }
-    return 1; // filled cell
+    return 1; /* filled cell */
 }
 
 void menger(int level)
 {
-    if (level < 0)
-        return;
-
     int size = pow(3, level);
-    for (int i = 0; i < size; i++)
+    int i, j;
+
+    for (i = 0; i < size; i++)
     {
-        for (int j = 0; j < size; j++)
+        for (j = 0; j < size; j++)
         {
             if (isCellFilled(i, j))
-            {
-                putchar('#');
-            }
+                printf("#");
             else
-            {
-                putchar(' ');
-            }
+                printf(" ");
         }
-        putchar('\n');
+        printf("\n");
     }
 }
