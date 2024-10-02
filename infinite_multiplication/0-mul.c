9151d0796c66d37548c58ec4de971fdc53a4821e
Mizuinu30 <juansilva.dvm@gmail.com>
2024-10-02 17:57:19 -0400

    adjusted for large numbers

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,247 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "holberton.h"
 
-/**
- * main - multiplies two positive numbers, passed in as args
- * @argc: int count of number of arguments provided to program
- * @argv: double pointer to array with program name and arguments
- *
- * Return: 0 if successful, exit status of 98 on failure
- */
-
-int main(int argc, char *argv[])
+void error(char **calculation, char *final)
 {
-	unsigned int digit_count1 = 0, digit_count2 = 0, max_digit = 0, min_digit = 0;
-	unsigned int row = 0, column = 0, start = 0, i = 0, j = 0;
-	int product = 0, remainder = 0;
-	char *num1 = NULL, *num2 = NULL, **calculation = NULL, *final = NULL;
-
-	if (argc != 3)
-		error();
-	digit_count1 = digit_check(argv[1]);
-	digit_count2 = digit_check(argv[2]);
-	if (digit_count1 == 0 || digit_count2 == 0)
-		error();
-	set_max_min(digit_count1, digit_count2, &max_digit, &min_digit,
-		    &num1, &num2, argv[1], argv[2]);
-	calculation = create_double(min_digit, min_digit + max_digit);
-	start = (min_digit + max_digit - 1);
-	for (i = min_digit, row = 0; i > 0; i--, row++)
-	{
-		for (j = max_digit, column = start; j > 0; j--, column--)
-		{
-			product = (num1[j - 1] - '0') * (num2[i - 1] - '0');
-			product += remainder;
-			calculation[row][column] = (product % 10) + '0';
-			remainder = product / 10;
-		}
-		calculation[row][column] = remainder + '0';
-		remainder = 0;
-		start--;
-	}
-	final = create_final(min_digit + max_digit, calculation);
-	for (i = 0; final[i]; i++)
-		_putchar(final[i]);
-	_putchar('\n');
-	for (row = 0; calculation[row]; row++)
-		free(calculation[row]);
-	free(calculation);
-	free(final);
-	return (0);
+    printf("Error\n");
+    if (calculation)
+        free(calculation);
+    if (final)
+        free(final);
+    exit(98);
 }
-
-/**
- * error - prints error message and exits program
- */
-
-void error(void)
-{
-	int i = 0;
-	char error[6] = "Error";
-
-	for (i = 0; error[i]; i++)
-		_putchar(error[i]);
-	_putchar('\n');
-	exit(98);
-}
-
-
-/**
- * digit_check - checks if number contains only digits and
- * counts number of digits
- *
- * @number: array of characters to check if digits
- *
- * Return: 0 on failure or number of digits in number
- */
 
 int digit_check(char *number)
 {
-	int i = 0;
+    int i = 0;
 
-	for (i = 0; number[i]; i++)
-	{
-		if (number[i] < '0' || number[i] > '9')
-			return (0);
-	}
-	return (i);
+    while (number[i])
+    {
+        if (!isdigit(number[i]))
+            return (0);
+        i++;
+    }
+    return (i);
 }
-
-/**
- * set_max_min - sets max and min numbers and digit counts
- * @digit_count1: count of digits in first number argument
- * @digit_count2: count of digits in second number argument
- * @max_digit: pointer to maximum number of digits
- * @min_digit: pointer to minimum number of digits
- * @num1: double pointer to number with maximum digits
- * @num2: double pointer to number with minimum digits
- * @arg1: pointer to first number argument
- * @arg2: pointer to second number argument
- */
 
 void set_max_min(unsigned int digit_count1, unsigned int digit_count2,
-		 unsigned int *max_digit, unsigned int *min_digit,
-		 char **num1, char **num2, char *arg1, char *arg2)
+                 unsigned int *max_digit, unsigned int *min_digit,
+                 char **num1, char **num2, char *arg1, char *arg2)
 {
-	if (digit_count1 > digit_count2)
-	{
-		*max_digit = digit_count1;
-		*num1 = arg1;
-		*min_digit = digit_count2;
-		*num2 = arg2;
-	}
-	else
-	{
-		*max_digit = digit_count2;
-		*num1 = arg2;
-		*min_digit = digit_count1;
-		*num2 = arg1;
-	}
+    if (digit_count1 >= digit_count2)
+    {
+        *max_digit = digit_count1;
+        *min_digit = digit_count2;
+        *num1 = arg1;
+        *num2 = arg2;
+    }
+    else
+    {
+        *max_digit = digit_count2;
+        *min_digit = digit_count1;
+        *num1 = arg2;
+        *num2 = arg1;
+    }
 }
-
-/**
- * create_double - mallocs and initializes a double pointer (2D array)
- *
- * @row_size: number of rows the double pointer should have
- * @column_size: number of columns each row should contain
- *
- * Return: the 2D array initialized with 0s
- */
 
 char **create_double(unsigned int row_size, unsigned int column_size)
 {
-	char **calculation = NULL;
-	unsigned int row = 0, column = 0;
+    char **calculation;
+    unsigned int i, j;
 
-	calculation = malloc(sizeof(char *) * (row_size + 1));
-	if (calculation == NULL)
-		error();
-	for (row = 0; row < row_size; row++)
-	{
-		calculation[row] = malloc(sizeof(char) * (column_size + 1));
-		if (calculation[row] == NULL)
-		{
-			for (; (row + 1) > 0; row--)
-				free(calculation[row]);
-			free(calculation);
-			error();
-		}
-		for (column = 0; column < column_size; column++)
-			calculation[row][column] = '0';
-		calculation[row][column] = '\0';
-	}
-	calculation[row] = NULL;
-	return (calculation);
+    calculation = malloc(row_size * sizeof(char *));
+    if (!calculation)
+        error(NULL, NULL);
+
+    for (i = 0; i < row_size; i++)
+    {
+        calculation[i] = malloc(column_size * sizeof(char));
+        if (!calculation[i])
+            error(calculation, NULL);
+        for (j = 0; j < column_size; j++)
+            calculation[i][j] = '0';
+    }
+    return (calculation);
 }
 
-/**
- * create_final - mallocs for final calculation of product and
- * sets values from calculations 2D array
- *
- * @size: maximum size needed for final array
- * @calculation: 2D array of all intermediary calculations
- *
- * Return: the final array with the final product value
- */
-
-char *create_final(unsigned int size, char **calculation)
+char *create_final(unsigned int size)
 {
-	char *final = NULL;
-	unsigned int row = 0, column = 0, sum = 0, remainder = 0;
+    char *final;
+    unsigned int i;
 
-	final = malloc(sizeof(char) * (size + 1));
-	if (final == NULL)
-	{
-		for (row = 0; calculation[row]; row++)
-			free(calculation[row]);
-		free(calculation);
-		error();
-	}
-	final[size] = '\0';
-	for (column = (size - 1); (column + 1) > 0; column--)
-	{
-		sum = 0;
-		for (row = 0; calculation[row]; row++)
-		{
-			sum += (calculation[row][column] - '0');
-		}
-		sum += remainder;
-		final[column] = (sum % 10) + '0';
-		remainder = sum / 10;
-	}
-	if (final[0] == '0')
-		final = final_realloc(final, calculation);
-	return (final);
+    final = malloc(size * sizeof(char));
+    if (!final)
+        error(NULL, NULL);
+
+    for (i = 0; i < size; i++)
+        final[i] = '0';
+
+    return (final);
 }
 
-/**
- * final_realloc - reallocates if there are any leading 0s
- * @final: pointer to calculated final product
- * @calculation: pointer to 2D array storing intermediary calculations
- *
- * Return: final product without leading 0s
- */
-char *final_realloc(char *final, char **calculation)
+char *final_realloc(char *final, unsigned int size)
 {
-    unsigned int i = 0, j = 0, leading = 0, new_size = 1;
-    char *final_realloc = NULL;
+    char *new_final;
+    unsigned int i;
 
-    for (i = 0; final[i]; i++)
-    {
-        if (leading)
-            new_size++;
-        else if (final[i] != '0')
-            leading = 1;
-    }
+    new_final = malloc(size * sizeof(char));
+    if (!new_final)
+        error(NULL, final);
 
-    final_realloc = malloc(sizeof(char) * (new_size + 1));
-    if (final_realloc == NULL)
-    {
-        for (i = 0; calculation[i]; i++)
-            free(calculation[i]);
-        free(calculation);
-        free(final);
-        error();  // Exits the program
-    }
-
-    final_realloc[new_size] = '\0';
-    leading = 0;
-    for (i = 0; final[i]; i++)
-    {
-        if (leading)
-        {
-            final_realloc[j] = final[i];
-            j++;
-        }
-        else if (final[i] != '0')
-        {
-            leading = 1;
-            final_realloc[j] = final[i];
-            j++;
-        }
-        else if (new_size == 1)
-            final_realloc[j] = '0';
-    }
+    for (i = 0; i < size; i++)
+        new_final[i] = final[i];
 
     free(final);
-    return (final_realloc);
+    return (new_final);
+}
+
+void multiply(char **calculation, unsigned int max_digit, unsigned int min_digit,
+              char *num1, char *num2)
+{
+    unsigned int i, j;
+    int carry, product, sum;
+
+    for (i = min_digit; i > 0; i--)
+    {
+        carry = 0;
+        for (j = max_digit; j > 0; j--)
+        {
+            product = (num2[i - 1] - '0') * (num1[j - 1] - '0') + carry;
+            sum = (calculation[min_digit - i][max_digit - j] - '0') + product;
+            carry = sum / 10;
+            calculation[min_digit - i][max_digit - j] = (sum % 10) + '0';
+        }
+        if (carry > 0)
+            calculation[min_digit - i][max_digit - j] = carry + '0';
+    }
+}
+
+void add(char **calculation, char *final, unsigned int max_digit,
+         unsigned int min_digit)
+{
+    unsigned int i, j;
+    int carry, sum;
+
+    for (i = 0; i < min_digit; i++)
+    {
+        carry = 0;
+        for (j = 0; j < max_digit + min_digit; j++)
+        {
+            sum = (final[j] - '0') + (calculation[i][j] - '0') + carry;
+            carry = sum / 10;
+            final[j] = (sum % 10) + '0';
+        }
+        if (carry > 0)
+            final[j] = carry + '0';
+    }
+}
+
+void print_final(char *final)
+{
+    int i = 0;
+
+    while (final[i] == '0')
+        i++;
+
+    if (final[i] == '\0')
+        _putchar('0');
+    else
+    {
+        while (final[i] != '\0')
+        {
+            _putchar(final[i]);
+            i++;
+        }
+    }
+    _putchar('\n');
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int digit_count1, digit_count2, max_digit, min_digit;
+    char *num1 = NULL, *num2 = NULL, **calculation = NULL, *final = NULL;
+
+    if (argc != 3)
+        error(NULL, NULL);
+
+    digit_count1 = digit_check(argv[1]);
+    digit_count2 = digit_check(argv[2]);
+
+    if (digit_count1 == 0 || digit_count2 == 0)
+        error(NULL, NULL);
+
+    set_max_min(digit_count1, digit_count2, &max_digit, &min_digit, &num1, &num2, argv[1], argv[2]);
+
+    calculation = create_double(min_digit, max_digit + min_digit);
+    final = create_final(max_digit + min_digit);
+
+    multiply(calculation, max_digit, min_digit, num1, num2);
+    add(calculation, final, max_digit, min_digit);
+    print_final(final);
+
+    free(final);
+    for (unsigned int i = 0; i < min_digit; i++)
+        free(calculation[i]);
+    free(calculation);
+
+    return (0);
 }
--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,26 +1,30 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/* INCLUDED LIBRARIES */
-#include <stdio.h>
-#include <stdlib.h>
-
 /* FUNCTION PROTOTYPES */
 /* writes single character to standard output */
 int _putchar(char c);
-/* prints error message and exits with error code (\*/
-void error(void);
+/* prints error message and exits with error code */
+void error(char **calculation, char *final);
 /* checks if number consists of only digits and returns digit count */
 int digit_check(char *number);
 /* sets the maximum and minimum number of digits and corresponding numbers */
 void set_max_min(unsigned int digit_count1, unsigned int digit_count2,
-		 unsigned int *max_digit, unsigned int *min_digit,
-		 char **num1, char **num2, char *arg1, char *arg2);
+         unsigned int *max_digit, unsigned int *min_digit,
+         char **num1, char **num2, char *arg1, char *arg2);
 /* mallocs and initializes a 2D array */
 char **create_double(unsigned int row_size, unsigned int column_size);
 /* mallocs and sets final array with sum of intermediary calculations */
-char *create_final(unsigned int size, char **calculation);
-/* reallocates final array to remove any leading zeros */
-char *final_realloc(char *final, char **calculation);
+char *create_final(unsigned int size);
+/* reallocs final array */
+char *final_realloc(char *final, unsigned int size);
+/* multiplies numbers and stores intermediary calculations */
+void multiply(char **calculation, unsigned int max_digit, unsigned int min_digit,
+              char *num1, char *num2);
+/* adds intermediary calculations and stores in final array */
+void add(char **calculation, char *final, unsigned int max_digit,
+         unsigned int min_digit);
+/* prints final product */
+void print_final(char *final);
 
-#endif
+#endif /* HOLBERTON_H */
