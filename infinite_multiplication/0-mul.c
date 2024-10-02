11eb5e21eb67f721554949a71617ef192f0e5251
Mizuinu30 <juansilva.dvm@gmail.com>
2024-10-02 15:17:30 -0400

    initial commit

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,75 +1,94 @@
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-int _isnumber(char *str);
-char *_callocX(int size);
-void multiply(char *num1, char *num2);
-
-int main(int argc, char **argv)
+/* Function to check if the string contains only digits */
+int is_digit(char *str)
 {
-    if (argc != 3 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
+    int i = 0;
+    while (str[i])
     {
-        printf("Error\n");
-        exit(98);
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+        i++;
     }
+    return (1);
+}
+
+/* Print error and exit */
+void print_error(void)
+{
+    char *error = "Error\n";
+    int i = 0;
+    
+    while (error[i])
+    {
+        _putchar(error[i]);
+        i++;
+    }
+    exit(98);
+}
+
+/* Main function to handle multiplication */
+int main(int argc, char *argv[])
+{
+    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+        print_error();
 
     multiply(argv[1], argv[2]);
-    return 0;
+    return (0);
 }
 
-int _isnumber(char *str)
-{
-    for (int i = 0; str[i]; i++)
-    {
-        if (!isdigit(str[i]))
-            return 0;
-    }
-    return 1;
-}
 
-char *_callocX(int size)
-{
-    char *res = calloc(size + 1, sizeof(char));
-    if (!res)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    return res;
-}
+#include <stdlib.h>
 
+/* Function to multiply two numbers represented as strings */
 void multiply(char *num1, char *num2)
 {
-    int l1 = strlen(num1);
-    int l2 = strlen(num2);
-    int lful = l1 + l2;
-    char *res = _callocX(lful);
-    int i, j, carry, n1, n2, sum;
+    int len1 = 0, len2 = 0, i, j, carry, n1, n2, sum;
+    int *result;
 
-    for (i = l1 - 1; i >= 0; i--)
+    /* Calculate lengths of the input strings */
+    while (num1[len1])
+        len1++;
+    while (num2[len2])
+        len2++;
+
+    /* Allocate memory for the result */
+    result = malloc(sizeof(int) * (len1 + len2));
+    if (!result)
+        exit(98);
+
+    for (i = 0; i < len1 + len2; i++)
+        result[i] = 0;
+
+    /* Perform the multiplication */
+    for (i = len1 - 1; i >= 0; i--)
     {
         carry = 0;
         n1 = num1[i] - '0';
-        for (j = l2 - 1; j >= 0; j--)
+
+        for (j = len2 - 1; j >= 0; j--)
         {
             n2 = num2[j] - '0';
-            sum = n1 * n2 + (res[i + j + 1] - '0') + carry;
+            sum = n1 * n2 + result[i + j + 1] + carry;
             carry = sum / 10;
-            res[i + j + 1] = (sum % 10) + '0';
+            result[i + j + 1] = sum % 10;
         }
-        res[i + j + 1] += carry;
+        result[i + j + 1] += carry;
     }
 
+    /* Skip leading zeros and print the result */
     i = 0;
-    while (i < lful && res[i] == '0')
+    while (i < len1 + len2 && result[i] == 0)
         i++;
 
-    if (i == lful)
-        printf("0\n");
+    if (i == len1 + len2)
+        _putchar('0');
     else
-        printf("%s\n", res + i);
-
-    free(res);
+    {
+        for (; i < len1 + len2; i++)
+            _putchar(result[i] + '0');
+    }
+    _putchar('\n');
+    free(result);
 }
--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,4 +1,9 @@
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
-#endif
+int is_digit(char *str);
+void multiply(char *num1, char *num2);
+void print_error(void);
+
+#endif /* HOLBERTON_H */
